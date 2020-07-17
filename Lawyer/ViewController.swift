3316ebd48208e9952bbc72568940c1737e2cc098
Aman <amankumarsodi@gmail.com>
2020-07-17 18:48:37 +0530

    Initial Commit

---
+++ Lawyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lawyer
+//
+//  Created by Aman Kumar on 17/07/20.
+//  Copyright © 2020 Hephateus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
