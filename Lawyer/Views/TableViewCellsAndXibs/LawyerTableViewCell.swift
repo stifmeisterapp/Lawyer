ce27e11322d20b5029eef4fddd36ec92093fa43d
Aman <amankumarsodi@gmail.com>
2020-08-05 20:30:54 +0530

    Old designs done in old branch with card cell.

--- Lawyer/Views/TableViewCellsAndXibs/LawyerTableViewCell.swift
+++ Lawyer/Views/TableViewCellsAndXibs/LawyerTableViewCell.swift
@@ -28,7 +28,6 @@ class LawyerTableViewCell: SBaseTableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
     //TODO: Setup cell implementation
