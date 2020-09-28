12d6f6296b29e8f2f9c5513c0b5980df97a812d0
Aman <amankumarsodi@gmail.com>
2020-09-28 13:02:06 +0530

    Upload icon completed

---
+++ Lawyer/Models/OrderDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  OrderDataModel.swift
+//  Lawyer
+//
+//  Created by Aman Kumar on 27/09/20.
+//  Copyright © 2020 Hephateus. All rights reserved.
+//
+
+import Foundation
+struct OrderDataModel {
+    var BookingDate:String
+    var BookingTime:String
+    var CategoryName:String
+    var CustomerEmail:String
+    var CustomerName:String
+    var CustomerPhone:String
+    var Id:String
+    var Query:String
+    var Status:String
+}
---
+++ Lawyer/Models/OrderStatusViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  OrderStatusViewModel.swift
+//  Lawyer
+//
+//  Created by Aman Kumar on 28/09/20.
+//  Copyright © 2020 Hephateus. All rights reserved.
+//
+
+import Foundation
+class OrderStatusViewModel {
+    var isExpended:Bool = Bool()
+    var isCompleted:Bool = Bool()
+    var title:String = String()
+    var values:[String] = [String]()
+    init(isExpended:Bool,isCompleted:Bool,title:String,values:[String]) {
+        self.isExpended = isExpended
+        self.isCompleted = isCompleted
+        self.title = title
+        self.values = values
+    }
+    
+}
